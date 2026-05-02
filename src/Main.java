874afa922fc1e4ad46e5f2415c8fdc5b102cf473
Lenovo <bommanajyothirmai@gmail.com>
2026-05-02 16:51:32 +0530

    INITIATED

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//    }
+//}
+
+//class Hello{
+//    public static void main(String[] args){
+//        System.out.println("Hello");
+//    }
+//}
+
+//class hello{
+//    public static void main(String[] args) {
+//        int num = 5;
+//        System.out.println(num);
+//    }
+//}
+
+class hai{
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 5;
+        int result = num1+num2;
+        System.out.println(result);
+    }
+}
