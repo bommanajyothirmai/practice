20db2cfb2b54f75c72a9103f2b080e60a18044c9
Lenovo <bommanajyothirmai@gmail.com>
2026-05-04 12:51:18 +0530

    BitwiseOperators

---
+++ src/BitwiseOperators.java
@@ -0,0 +1,12 @@
+public class BitwiseOperators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        System.out.println("a&b: "+(a&b));
+        System.out.println("a|b: "+(a|b));
+        System.out.println("a^b: "+(a^b));
+        System.out.println("~a: "+ ~a);
+        System.out.println("a<<b: "+(a<<b));
+        System.out.println("a>>b: "+(a>>b));
+    }
+}
