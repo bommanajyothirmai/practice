f745f4daccfc271c5f3fd64c04fc938b6b90dcb7
Lenovo <bommanajyothirmai@gmail.com>
2026-05-04 17:57:31 +0530

    AreaOfaCircle

---
+++ src/AreaOfaCircle.java
@@ -0,0 +1,8 @@
+public class AreaOfaCircle {
+    public static void main(String[] args){
+        int radius = 5;
+        double pi = 3.14;
+        double area = (pi * radius * radius);
+        System.out.println(area);
+    }
+}
