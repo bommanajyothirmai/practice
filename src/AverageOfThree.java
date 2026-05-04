faf86a18e497f7324549b7e3cb8ed2f4b272ef7d
Lenovo <bommanajyothirmai@gmail.com>
2026-05-04 18:10:49 +0530

    AverageOfThree

---
+++ src/AverageOfThree.java
@@ -0,0 +1,9 @@
+public class AverageOfThree {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int average = (a+b+c)/3;
+        System.out.println(average);
+    }
+}
