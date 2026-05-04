bab86f3ba9dc7021c1d24901089d51c14d847a27
Lenovo <bommanajyothirmai@gmail.com>
2026-05-04 11:46:29 +0530

    Relational Operators

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
- class ArithmeticOperators {
-    public static void main(String[] args) {
+class RelationalOperators{             //<,>,<=,>=,==,!=
+    public static void main(String[] args){
         int a = 10;
         int b = 5;
 
-        int sum = a+b;
-        int minus = a-b;
-        int multiply = a*b;
-        int division = a/b;
-        int remainder = a%b;
-
-        System.out.println("Addition: " + sum);
-        System.out.println("Subtraction: " + minus);
-        System.out.println("multiply: " + multiply);
-        System.out.println("division: " + division);
-        System.out.println("modulus: " + remainder);
+        System.out.println("a<b: " + (a<b));
+        System.out.println("a>b: " + (a>b));
+        System.out.println("a<=b: " + (a<=b));
+        System.out.println("a>=b: " + (a>=b));
+        System.out.println("a==b: " + (a==b));
+        System.out.println("a!=b: " + (a!=b));
     }
- }
+}
