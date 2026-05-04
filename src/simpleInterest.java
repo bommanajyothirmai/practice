1c1c67bc28707ca398bd25e71fc5324fd1b86389
Lenovo <bommanajyothirmai@gmail.com>
2026-05-04 15:21:31 +0530

    simpleInterest

---
+++ src/simpleInterest.java
@@ -0,0 +1,9 @@
+public class simpleInterest {
+    public static void main(String[] args){
+        int principal = 50000;
+        double rate = 2;
+        double time = 2;
+        double si = (principal*rate*time)/100;
+        System.out.println(si);
+    }
+}
