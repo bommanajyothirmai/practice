4b122743e33254b8b0ffccf35b41fcca22c6f6eb
Lenovo <bommanajyothirmai@gmail.com>
2026-05-05 17:14:10 +0530

    OddNumbers

---
+++ src/OddNumbers.java
@@ -0,0 +1,10 @@
+public class OddNumbers {
+    public static void main(String[] args){
+        int sum = 0;
+        for( int i = 1 ; i <= 10 ; i ++){
+            if(i % 2 != 0){
+                System.out.println("Odd numbers are: "+ i);
+            }
+        }
+    }
+}
