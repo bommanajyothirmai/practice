b63d7b9970230aaccdb9cf1304ad982e6318655d
Lenovo <bommanajyothirmai@gmail.com>
2026-05-05 12:22:14 +0530

    DivisibleByTwoAndThree

---
+++ src/DivisibleByTwoAndThree.java
@@ -0,0 +1,10 @@
+public class DivisibleByTwoAndThree {
+    public static void main(String[] args) {
+        int num = 6;
+        if (num % 2 == 0 && num % 3 == 0) {
+            System.out.println("Number Divisible By Two and Three");
+        } else {
+            System.out.println("Number Not Divisible By Two and Three");
+        }
+    }
+}
