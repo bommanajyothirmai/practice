4721eb003ce181af5504a9312db070f4d44bdef4
Lenovo <bommanajyothirmai@gmail.com>
2026-05-05 09:31:03 +0530

    SwapTwoNumbers

---
+++ src/SwapNumbers.java
@@ -0,0 +1,11 @@
+public class SwapNumbers {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        int numchange = a;
+        a = b;
+        b = numchange;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
