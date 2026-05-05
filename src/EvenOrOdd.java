49052506e24e6b6bd25ed5c7e942540b2fac41d4
Lenovo <bommanajyothirmai@gmail.com>
2026-05-05 12:15:03 +0530

    EvenOrOdd

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,10 @@
+public class EvenOrOdd {
+    public static void main(String[] args){
+        int a = 10;
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
