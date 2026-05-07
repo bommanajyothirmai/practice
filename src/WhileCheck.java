f8e3d68d167e1c66dd880ad2b88a9eec5ef35151
Lenovo <bommanajyothirmai@gmail.com>
2026-05-07 15:49:03 +0530

    while loop

---
+++ src/WhileCheck.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class WhileCheck {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
