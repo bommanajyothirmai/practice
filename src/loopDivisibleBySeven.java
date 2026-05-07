b2b5153684bedfdbfe7c839e912bb31c25755533
Lenovo <bommanajyothirmai@gmail.com>
2026-05-07 16:05:26 +0530

    loopDivisibleBySeven

---
+++ src/loopDivisibleBySeven.java
@@ -0,0 +1,13 @@
+public class loopDivisibleBySeven {
+    public static void main(String[] args) {
+        int i = 150;
+        while(i<=200){
+            i++;
+            if(i%7==0){
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
